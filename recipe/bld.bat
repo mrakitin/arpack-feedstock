0c9e585ccfadf5df1e6128394d2517b0104485b3
Julien Schueller <schueller@phimeca.com>
2018-11-24 10:59:10 +0100

    Add lib test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,8 +8,6 @@ mkdir build && cd build
 cmake -G "MinGW Makefiles" ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
-  -DCMAKE_C_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4" ^
-  -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4" ^
   ..
 if errorlevel 1 exit 1
 
